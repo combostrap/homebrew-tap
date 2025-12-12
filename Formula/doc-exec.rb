943f9a11099a6ea1ce41395fe0bebd0c72e3720f
Nicolas GERARD <gerardnico@gmail.com>
2025-12-12 21:40:11 +0100

    fix: doc-exec bad jre

--- Formula/doc-exec.rb
+++ Formula/doc-exec.rb
@@ -2,42 +2,17 @@
 
 class DocExec < Formula
   desc "ComboStrap Doc Exec"
-  homepage "https://github.com/ComboStrap/doc-exec"
+  homepage "https://github.com/combostrap/doc-exec"
   version "1.0.0"
   license "Functional Source License, Version 1.1, MIT Future License"
+  url "https://github.com/combostrap/doc-exec/releases/download/v1.0.0/combostrap-doc-exec-1.0.0-nojre.zip"
+  sha256 "00d660779b8298d9d43df80853b2d41a4491835acff60bd36fb8c53c92d03505"
 
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/combostrap/doc-exec/releases/download/v1.0.0/combostrap-doc-exec-1.0.0-jre-linux-arm64.zip"
-    sha256 "125f912bc74c573fdef076d4ac85b489a5ce7a74541fe114c6469d324784de86"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/combostrap/doc-exec/releases/download/v1.0.0/combostrap-doc-exec-1.0.0-jre-linux-x64.zip"
-    sha256 "1044a2c84ad3d2bf0f1bc165927d798a01496c86d121ac91be1f52bb9d63beac"
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/combostrap/doc-exec/releases/download/v1.0.0/combostrap-doc-exec-1.0.0-jre-macos-arm64.zip"
-    sha256 "f07b62cbaff8d18bdc49529c6a5d8cb1f42c51cce8f415e4a63adb3fc02b36d2"
-  end
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/combostrap/doc-exec/releases/download/v1.0.0/combostrap-doc-exec-1.0.0-jre-macos-x64.zip"
-    sha256 "c29a0817765af3fd272ded2e6b2a83aa01ab9dae54087eda3175c7552564b9d4"
-  end
-
+  depends_on "openjdk@17"
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/doc-exec" => "doc-exec"
-  end
-
-  def post_install
-    if OS.mac?
-      Dir["#{libexec}/lib/**/*.dylib"].each do |dylib|
-        chmod 0664, dylib
-        MachO::Tools.change_dylib_id(dylib, "@rpath/#{File.basename(dylib)}")
-        MachO.codesign!(dylib)
-        chmod 0444, dylib
-      end
-    end
   end
 
   test do
